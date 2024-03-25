0695a554769251f14d1203157e105fb256228f3b
Astyell <etoiledeplante@gmail.com>
2024-03-25 15:53:26 +0100

    Ajout Page de connexion

--- frontend/php/connexion.php
+++ frontend/php/connexion.php
@@ -7,7 +7,24 @@
 </head>
 <body>
 
-	
+	<fieldset>
+            <h2><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="hide">Connexion</a></h2>
+			<form action="sae203.php" method="post">
+
+				<p>
+					<strong>Email</strong> : <input type="text" id="email" name="email" placeholder="Entrez votre email..." required autofocus></br></br>
+					<strong>Mot de passe</strong> : <input type="password" id="mdp" name="mdp" placeholder="Entrez votre mot de passe..." required></br><br>
+				</p>
+                <input type="submit" id="button" value="Connexion"></br>
+                
+                <p class="small">Vous n'avez pas de compte?</p><a href="inscription.php" class ="small">Inscrivez-vous !</a>
+
+			</form>
+			
+        <div id="error">
+            
+        </div>
+	</fieldset>
 	
 </body>
 </html>
