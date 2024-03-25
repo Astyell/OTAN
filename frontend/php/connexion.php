96db4c956153f70d282d7fae67abaa1a5acabbeb
Astyell <etoiledeplante@gmail.com>
2024-03-25 16:27:02 +0100

    Page de connexion html

--- frontend/php/connexion.php
+++ frontend/php/connexion.php
@@ -3,21 +3,20 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../css/connexion.css">
 	<title>S.A.T.A.N. - Connexion</title>
 </head>
 <body>
 
 	<fieldset>
-            <h2><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="hide">Connexion</a></h2>
+            <h2><a href="https://www.youtube.com/watch?v=Oe3FG4EOgyU" class="hide">Connexion</a></h2>
 			<form action="sae203.php" method="post">
 
 				<p>
-					<strong>Email</strong> : <input type="text" id="email" name="email" placeholder="Entrez votre email..." required autofocus></br></br>
+					<strong>Identifiant</strong> : <input type="text" id="id" name="id" placeholder="Entrez votre identifiant..." required autofocus></br></br>
 					<strong>Mot de passe</strong> : <input type="password" id="mdp" name="mdp" placeholder="Entrez votre mot de passe..." required></br><br>
 				</p>
                 <input type="submit" id="button" value="Connexion"></br>
-                
-                <p class="small">Vous n'avez pas de compte?</p><a href="inscription.php" class ="small">Inscrivez-vous !</a>
 
 			</form>
 			
