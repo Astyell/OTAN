c24142cce5d6055d5cfca6aba886023e6a0a58ea
julia123456789037 <julia037.filia@gmail.com>
2024-03-26 16:44:03 +0100

    ajout TODO

--- frontend/php/fctAux.inc.php
+++ frontend/php/fctAux.inc.php
@@ -7,7 +7,7 @@
 		echo "<head>\n";
 		echo "<meta charset='UTF-8'>\n";
 		echo "<meta name='Author' lang='fr' content='Justine BONDU Sébastien CHAMPVILLARD Alizéa LEBARON Matéo SA'  />\n";
-        echo "<link rel='stylesheet' href='style.css' type='text/css' />\n";
+        echo "<link rel='stylesheet' href='../css/style.css' type='text/css' />\n";
 		echo "<title>Menu</title>\n";
 		echo "</head>\n";
         echo "<body>\n";
@@ -20,12 +20,13 @@
 
     function headerMenu()
     {
+        //TODO : remplacer "provisoir_imm" par les vrai images
         echo "<header >";
         echo "<div class=\"menuBar\">";
-        echo "<a href=\"importation.html\" class=\"imp\"> <img src=\"provisoir_imm.png\" title=\"importation\" alt=\"importationLogo\"></a>";
-        echo "<a href=\"exportation.html\" class=\"Exp\"> <img src=\"provisoir_imm.png\" title=\"exportation\" alt=\"exportationLogo\"></a>";
-        echo "<a href=\"templateAvisPoursuiteEtud.html\" class=\"avis\"> <img src=\"provisoir_imm.png\" title=\"template Avis Poursuite Etude\" alt=\"poursuiteEtudLogo\"></a>";
-        echo "<a href=\"connexion.html\" class=\"quiter\"> <img src=\"provisoir_imm.png\" title=\"déconnexion\" alt=\"quitterLogo\"></a>";
+        echo "<a href=\"importation.html\" class=\"imp\"> <img src=\"../img/provisoir_imm.png\" title=\"importation\" alt=\"importationLogo\"></a>";
+        echo "<a href=\"exportation.html\" class=\"Exp\"> <img src=\"../img/provisoir_imm.png\" title=\"exportation\" alt=\"exportationLogo\"></a>";
+        echo "<a href=\"templateAvisPoursuiteEtud.html\" class=\"../img/avis\"> <img src=\"logo_avi.png\" title=\"template Avis Poursuite Etude\" alt=\"poursuiteEtudLogo\"></a>";
+        echo "<a href=\"connexion.html\" class=\"quiter\"> <img src=\"../img/provisoir_imm.png\" title=\"déconnexion\" alt=\"quitterLogo\"></a>";
         echo "</div>";
         echo "<div id=\"ligne\"></div>";
         echo "</header>";
