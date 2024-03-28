78aadb65b7085b343629268549693cfedfac549c
julia123456789037 <julia037.filia@gmail.com>
2024-03-28 16:25:50 +0100

    css base

--- frontend/php/choixFichier.php
+++ frontend/php/choixFichier.php
@@ -1,30 +1,28 @@
 <?php
 	include ("fctAux.inc.php");
-	require '../../Controleur.php';
-    session_start();
+	session_start();
 
 	enTete1_2();
 	echo "<title>Sélectionner un fichier</title>";
 	enTete2_2();
-	echo "<header >\n";
+	echo "<header class=\"header\">\n";
 	echo "<p> Importer </p>";
 	echo "</header>\n";
 	echo "<br>";
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
-
-
-    echo "<br>";
-    echo "<section class=\"encad\">";
-    corps();
-    echo "</section>";
-
+	echo "<br>";
+	echo "<section class=\"encad\">";
+	corps();
+	echo "</section>";
 	echo "<br>";
 	pied();
 
-    function corps() 
+	function corps()
 	{
 		selectionFichier();
+
+		require '../../Controleur.php';
 
 		// Vérifier si le formulaire a été soumis et si un fichier a été envoyé
 		if(isset($_POST['submit']) && isset($_FILES['file'])) 
@@ -44,9 +42,8 @@
 		echo '<form action="" method="post" enctype="multipart/form-data">
 			<label for="file">Sélectionner un fichier :</label><br>
 			<input type="file" id="file" name="file" accept=".xlsx"><br><br>
-			<input type="submit" name="submit" value="envListe">
+			<input type="submit" name="submit" value="Envoyer">
 		</form>';
-}
+	}
+
 ?>
-
-
