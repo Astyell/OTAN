226525b5061ffe81109a902640d3157eda445eac
Astyell <etoiledeplante@gmail.com>
2024-03-29 09:08:44 +0100

    V1.0.1 visualisation.php

--- frontend/php/fctAux.inc.php
+++ frontend/php/fctAux.inc.php
@@ -1,17 +1,4 @@
 <?php
-	function enTeteVisuali() 
-	{
-        echo "<!DOCTYPE html>\n";
-        echo "<html lang='fr'>\n";
-		echo "<head>\n";
-		echo "<meta charset='UTF-8'>\n";
-		echo "<meta name='Author' lang='fr' content='Justine BONDU Sébastien CHAMPVILLARD Alizéa LEBARON Matéo SA'  />\n";
-        echo "<link rel='stylesheet' href='../css/style.css' type='text/css' />\n";
-		echo "<title>Menu</title>\n";
-		echo "</head>\n";
-        echo "<body>\n";
-        echo "<br>\n";
-	}
 
     function enTete1_2() 
 	{
@@ -31,50 +18,6 @@
         echo "<br>\n";
 	}
 
-    function headerMenuVisuali()
-    {
-        echo "<header class=\"headerVisu\">\n";
-        echo "<div class=\"menuBar\">\n";
-        echo "<a href=\"importation.html\" class=\"imp\"> <img src=\"../img/logo_telecharger.png\" title=\"importation\" alt=\"importationLogo\"></a>\n";
-        echo "<a href=\"exportation.html\" class=\"Exp\"> <img class=\"imaInvers\" src=\"../img/logo_telecharger.png\" title=\"exportation\" alt=\"exportationLogo\"></a>\n";
-        echo "<a href=\"templateAvisPoursuiteEtud.html\" class=\"../img/avis\"> <img src=\"logo_avi.png\" title=\"template Avis Poursuite Etude\" alt=\"poursuiteEtudLogo\"></a>\n";
-        echo "<a href=\"connexion.html\" class=\"quiter\"> <img src=\"../img/logo_deconnexion.png\" title=\"déconnexion\" alt=\"quitterLogo\"></a>\n";
-        echo "</div>\n";
-        echo "<hr class=\"ligne\">\n";
-        echo "</header>\n";
-    }
-
-    function navMenuVisuali()//TODO : retacher une fonction celonn le bouton cliquer pour changer l'affichage
-    {
-        /* Ce menu est composé d'un tableau séparé en 6 parti chacune de ses parties représente un semestre.
-		Chaque semestre comporte un bouton jury, et les semestres impairs ont, en plus un bouton commission.*/
-        echo '<!-- Ce menu est composé d\'un tableau séparé en 6 parti chacune de ses parties représente un semestre. -->' + "\n";
-		echo '<!-- Chaque semestre comporte un bouton jury, et les semestres impairs ont, en plus un bouton commission. -->' + "\n";
-		echo "<nav class=\"menuVue\" >\n";
-        echo "<table  >\n";
-		echo "<tbody>\n<tr>\n";
-		echo "<TD ROWSPAN=2>S1</TD> \n";
-		echo "<td ><button class=\"BcommJur\">Commission</button></td>\n";
-		echo "</tr> \n<tr>\n";
-		echo "<td ><button class=\"BcommJur\">Jury</button></td>\n";
-		echo "</tr> \n<tr> \n<td>S2</td>\n";
-		echo "<td ><button class=\"BJurPair\">Jury</button></td>\n";
-		echo "</tr> \n<tr> \n<TD ROWSPAN=2>S3</TD> \n";
-		echo "<td ><button class=\"BcommJur\">Commission</button></td>\n";
-		echo "</tr> \n<tr>\n";
-		echo "<td ><button class=\"BcommJur\">Jury</button></td>\n";
-		echo "</tr> \n<tr> \n<td>S4</td>\n";
-		echo "<td ><button class=\"BJurPair\">Jury</button></td>\n";
-		echo "</tr> \n<tr>\n <TD ROWSPAN=2>S5</TD> \n";
-		echo "<td ><button class=\"BcommJur\">Commission</button></td>\n";
-		echo "</tr>\n <tr>\n";
-		echo "<td ><button class=\"BcommJur\">Jury</button></td>\n";
-		echo "</tr>\n <tr>\n <td>S6</td>\n";
-		echo "<td ><button class=\"BJurPair\" >Jury</button></td>\n";
-		echo "</tr>\n</tbody>\n";
-        echo "</table > \n";
-        echo "</nav>\n";
-    }
 
     function deuxBoutons($gauche,$droite)  
     {
--- frontend/php/visualisation.php
+++ frontend/php/visualisation.php
@@ -1,29 +1,106 @@
-
 <?php
+	/** connexion.php
+	* @author  : Alizéa Lebaron, Justine BONDU
+	* @since   : 26/03/2024
+	* @version : 1.0.1 - 29/03/2024
+	*/
+
+	// Affichage des erreurs
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	// Importation
 	include ("fctAux.inc.php");
-	require ("DB.inc.php");
+	require ("../../backend/php/DB/DB.inc.php");
+
+	// Début de la session
     session_start();
 
-	enTeteVisuali();
-    headerMenuVisuali();
-    navMenuVisuali();
-	echo "<br>";
-	echo "<br>";
+	// Vérification que la session existe bien
+	if (!isset($_SESSION['id'])) 
+	{
+        header('Location: connexion.php');
+        exit();
+    }
 
+	// Récupération des données
+	$ID    = $_SESSION [   'id'];
+	$droit = $_SESSION ['droit'];
+?>
 
-    echo "<br>";
-    echo "<section class=\"corpV\">";
-    corps();
-    echo "</section>";
+<!DOCTYPE html>
+<html lang='fr'>
+
+<head>
+	<meta charset='UTF-8'>
+	<meta name='Author' lang='fr' content='Justine BONDU Sébastien CHAMPVILLARD Alizéa LEBARON Matéo SA'/>
+	<link rel='stylesheet' href='../css/visualisation.css' type='text/css' />
+	<title>Menu</title>
+</head>
+
+<body>
+
+	<header>
+		<nav class="navbar">
+			<ul>
+				<li><img src="../img/icone/import.png" alt="import">         <a href="choixFichier.php">Importer</a></li>
+				<li><img src="../img/icone/export.png" alt="export">         <a href="#">Exporter</a></li>
+				<li><img src="../img/icone/doc.png"    alt="poursuite etude"><a href="#">Fiche poursuite d'étude</a></li>
+				<li><img src="../img/icone/power.png"  alt="deconnexion">    <a href="deconnexion.php">Déconnexion</a></li>
+			</ul>
+		</nav>
+    </header>
+	
+	<!-- Ce menu est composé d\'un tableau séparé en 6 parti chacune de ses parties représente un semestre. -->
+	<!-- Chaque semestre comporte un bouton jury, et les semestres impairs ont, en plus un bouton commission. -->
+	<nav class="menuVue" >
+		<table>
+			<tr>
+				<td rowspan=2>S1</td> 
+				<td><button class="BcommJur">Commission</button></td>
+			</tr> 
+			
+			<tr>
+				<td ><button class="BcommJur">Jury</button></td>
+			</tr> 
+			
+			<tr> 
+				<td>S2</td>
+				<td ><button class="BJurPair">Jury</button></td>
+			</tr> 
+			
+			<tr> 
+				<td ROWSPAN=2>S3</td> 
+				<td ><button class="BcommJur">Commission</button></td>
+			</tr>
+
+			<tr>
+				<td ><button class="BcommJur">Jury</button></td>
+			</tr> 
+			
+			<tr> 
+				<td>S4</td>
+				<td ><button class="BJurPair">Jury</button></td>
+			</tr> 
+			
+			<tr> 
+				<td ROWSPAN=2>S5</td> 
+				<td ><button class="BcommJur">Commission</button></td>
+			</tr> 
+			
+			<tr>
+				<td ><button class="BcommJur">Jury</button></td>
+			</tr> 
+
+			<tr> 
+				<td>S6</td>
+				<td ><button class="BJurPair" >Jury</button></td>
+			</tr>
+		</table> 
+    </nav> 
+
+</body>
+</html>
 
 	
-	echo "<br>";
-	pied();
 
-    function corps() 
-	{
-		$db = DB::getInstance();
-        echo "chalut, faut me compléter";
-        
-	}
-?>
