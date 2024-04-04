ea1a96a1a0993d738c306db851631b0fb3b92ec1
julia123456789037 <julia037.filia@gmail.com>
2024-04-04 11:30:47 +0200

    identation

--- frontend/php/exporter.php
+++ frontend/php/exporter.php
@@ -34,10 +34,10 @@
 	}
 	iset();
 	enTete1_2();
-	echo "\t<link rel='stylesheet' href='../css/header.css'  type='text/css' />\n";
-	echo "\t<link rel='stylesheet' href='../css/impoExp.css' type='text/css'/>\n";
-	echo "\t<link rel='stylesheet' href='../css/footer.css'  type='text/css' />\n";
-	echo "\t<title>O.T.A.N. - Exporter</title>\n";
+	echo "\t \t<link rel='stylesheet' href='../css/header.css'  type='text/css' />\n";
+	echo "\t \t<link rel='stylesheet' href='../css/impoExp.css' type='text/css'/>\n";
+	echo "\t \t<link rel='stylesheet' href='../css/footer.css'  type='text/css' />\n";
+	echo "\t \t<title>O.T.A.N. - Exporter</title>\n";
 	enTete2_2();
 
 	// Afficher le header en fonction de l'utilisateur
@@ -49,12 +49,12 @@
 	$lstSem = $db->getAllSemestre();
 	sort($lstAnn);
 	sort($lstSem);
-	echo "\t<h1>Exporter</h1>\n";
-	echo "\t<section class=\"encad\">\n";
+	echo "\n \t \t<h1>Exporter</h1>\n";
+	echo "\t \t<section class=\"encad\">\n";
 	genererTableau($lstAnn, $lstSem);
-	echo "\t</section>\n";
+	echo "\t \t</section>\n";
 	pied();
-	echo "\t</body>\n</html>\n";
+	echo "\n\t</body>\n</html>\n";
 
 
 
@@ -102,47 +102,47 @@
 
 	function genererTableau($lstAnn, $lstSem)
 	{
-		echo "\t<p> Veillez exporter les fichier un par un. </p>\n<br>\n";
-		echo "<form method='post' action=''>\n";
-		echo "<label for=\"annee\">Sélectionner une année :</label>\n";
-		echo "<select name=\"annee\">\n";
-		foreach ($lstAnn as $annee) { echo "<option value='".$annee->getId_annee()."'>".$annee->getId_annee()."</option>\n"; }
-		echo "</select>\n";
-		echo "</select><input type=\"submit\" value=\"Consulter\">";
-		echo "</form>\n<br><br>";
+		echo "\t \t \t<p> Veillez exporter les fichier un par un. </p><br>\n";
+		echo "\t \t \t<form method='post' action=''>\n";
+		echo "\t \t \t \t<label for=\"annee\">Sélectionner une année :</label>\n";
+		echo "\t \t \t \t<select name=\"annee\">\n";
+		foreach ($lstAnn as $annee) { echo "\t \t \t \t \t<option value='".$annee->getId_annee()."'>".$annee->getId_annee()."</option>\n"; }
+		echo "\t \t \t \t</select>\n";
+		echo "\t \t \t \t<input type=\"submit\" value=\"Consulter\">";
+		echo "\t \t \t</form><br><br>";
 		$anneeSelectionnee = isset($_POST['annee']) ? $_POST['annee'] : null;
 		if ($anneeSelectionnee === null && count($lstAnn) > 0) {
 			$anneeSelectionnee = $lstAnn[0]->getId_annee();
 		}
-		echo "<form method='post' action=''>\n";
-		echo "<table >\n";
+		echo "\t \t \t<form method='post' action=''>\n";
+		echo "\t \t \t \t<table >\n";
 
 		foreach ($lstSem as $semestre) 
 		{
 			if ($semestre->getId_annee() == $anneeSelectionnee)
 			{
-				echo "<tr>\n<th >S".$semestre->getId_semestre()."</th > \n<th >Excel</th > \n<th >Word</th > \n<th >PDF</th >\n</tr>\n";
-				echo "<tr>\n";
-				echo "<td>fichier commission</td>\n";
-				echo "<td class='case'><input type='checkbox' class='caseC' name='commission_".$semestre->getId_semestre()."_0'></td>\n";
-				for ($j = 1; $j < 3; $j++) { echo "<td class='case'><input type='checkbox' class='caseInnac' name='commission_".$semestre->getId_semestre()."_".$j."' disabled></td>\n"; }
-				echo "</tr>\n<tr>\n";
-				echo "<td>fichier jury</td>\n";
-				echo "<td class='case'><input type='checkbox' class='caseC' name='jury_".$semestre->getId_semestre()."_0'></td>\n";
-				for ($k = 1; $k < 3; $k++) { echo "<td class='case'><input type='checkbox' class='caseInnac' name='jury_".$semestre->getId_semestre()."_".$k."' disabled></td>\n"; }
-				echo "</tr>\n";
+				echo "\t \t \t \t \t<tr>\n\t \t \t \t \t \t<th >S".$semestre->getId_semestre()."</th > \n\t \t \t \t \t \t<th >Excel</th > \n\t \t \t \t \t \t<th >Word</th > \n\t \t \t \t \t \t<th >PDF</th >\n\t \t \t \t \t</tr>\n";
+				echo "\t \t \t \t \t<tr>\n";
+				echo "\t \t \t \t \t \t<td>fichier commission</td>\n";
+				echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseC' name='commission_".$semestre->getId_semestre()."_0'></td>\n";
+				for ($j = 1; $j < 3; $j++) { echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseInnac' name='commission_".$semestre->getId_semestre()."_".$j."' disabled></td>\n"; }
+				echo "\t \t \t \t \t</tr>\n\t \t \t \t \t<tr>\n";
+				echo "\t \t \t \t \t \t<td>fichier jury</td>\n";
+				echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseC' name='jury_".$semestre->getId_semestre()."_0'></td>\n";
+				for ($k = 1; $k < 3; $k++) { echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseInnac' name='jury_".$semestre->getId_semestre()."_".$k."' disabled></td>\n"; }
+				echo "\t \t \t \t \t</tr>\n";
 			}
 		}
-		echo "<tr>\n";
-		echo "<tr >\n<th COLSPAN=2>Autre</th >  \n<th >Word</th > \n<th >PDF</th >\n</tr>\n";
-		echo "<td COLSPAN=2>Avis de poursuite d'étude</td>\n";
-		echo "<td class='case'><input type='checkbox' class='caseInnac' name='AvisPoursuiteEtude_0' disabled></td>\n";
-		echo "<td class='case'><input type='checkbox' class='caseC' name='AvisPoursuiteEtude_1'></td>\n";
-		echo "</tr>\n";
-		echo "</table>\n<br>\n";
-		echo "Attention, les avis de poursuite d'étude sont nominatifs et nécessitent de modifier le modèle en amont.\n<br><br>\n";
-		echo "<input type=\"submit\" class=\"Valid\" name=\"valider\" value=\"Valider\">\n";
-		echo "</form>\n";
+		echo "\t \t \t \t \t<tr>\n";
+		echo "\t \t \t \t \t<tr >\n\t \t \t \t \t \t<th COLSPAN=2>Autre</th >  \n\t \t \t \t \t \t<th >Word</th > \n\t \t \t \t \t \t<th >PDF</th >\n\t \t \t \t \t</tr>\n";
+		echo "\t \t \t \t \t \t<td COLSPAN=2>Avis de poursuite d'étude</td>\n";
+		echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseInnac' name='AvisPoursuiteEtude_0' disabled></td>\n";
+		echo "\t \t \t \t \t \t<td class='case'><input type='checkbox' class='caseC' name='AvisPoursuiteEtude_1'></td>\n";
+		echo "\t \t \t \t \t</tr>\n";
+		echo "\t \t \t \t</table><br>\n";
+		echo "\t \t \t \t<p>Attention, les avis de poursuite d'étude sont nominatifs et nécessitent de modifier le modèle en amont.</p>\n";
+		echo "\t \t \t \t<input type=\"submit\" class=\"Valid\" name=\"valider\" value=\"Valider\">\n";
+		echo "\t \t \t</form>\n";
 	}
 	
 ?>
